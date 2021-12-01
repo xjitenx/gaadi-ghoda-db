5067065e6ed6f137e140e15aeb0eba53c44a0cbd
Jitender Sharma <jitender.sharma@energyexemplar.com>
2021-12-01 13:46:00 +0530

    first commit

---
+++ Table Definitions/tbl_org.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS tbl_org (
+    org_id uuid DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
+    org_person_first_name VARCHAR(200) NOT NULL,
+    org_person_last_name VARCHAR(200) NOT NULL,
+    org_email_id VARCHAR(200) NOT NULL,
+    org_contact_no VARCHAR(10) NOT NULL,
+    org_address VARCHAR(100) NOT NULL,
+	org_country VARCHAR(50) NOT NULL,
+	org_state VARCHAR(50) NOT NULL,
+	org_city VARCHAR(50) NOT NULL,
+	org_zip_code VARCHAR(10) NOT NULL,
+    org_enabled_YN CHAR(1) NOT NULL
+);
