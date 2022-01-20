e25698fb8c80b961a35202820ace052b502cfbd3
Jitender Sharma <jitender.sharma@energyexemplar.com>
2022-01-20 23:35:37 +0530

    changes to remove user alias from db

--- Misc/Queries.sql
+++ Misc/Queries.sql
@@ -13,8 +13,8 @@ org_id, bookie_subscription_count, broker_subscription_count, subscription_start
  
  
 	INSERT INTO public.tbl_user (
-user_login_date_time, org_id, user_alias, user_first_name, user_last_name, user_email_id, user_contact_no, user_enabled_yn, user_login_allowed_yn, user_login_fail_tries, bookie_access_yn, broker_access_yn, user_password) VALUES (
-'2021-11-06 12:33:24.847166+05:30'::timestamp without time zone, 'd87005c6-9a10-41be-a83a-bdf8f52d3e90'::uuid, 'xjitenx'::character varying, 'Jitender'::character varying, 'Sharma'::character varying, 'jitender.gow@gmail.com'::character varying, '1231231231'::character varying, 'Y', 'Y', '0'::smallint, 'Y', 'N', 'poplol123'::character varying)
+user_login_date_time, org_id, user_first_name, user_last_name, user_email_id, user_contact_no, user_enabled_yn, user_login_allowed_yn, user_login_fail_tries, bookie_access_yn, broker_access_yn, user_password) VALUES (
+'2021-11-06 12:33:24.847166+05:30'::timestamp without time zone, 'd87005c6-9a10-41be-a83a-bdf8f52d3e90'::uuid, 'Jitender'::character varying, 'Sharma'::character varying, 'jitender.gow@gmail.com'::character varying, '1231231231'::character varying, 'Y', 'Y', '0'::smallint, 'Y', 'N', 'poplol123'::character varying)
  returning user_id;
  
  
--- Table Definitions/tbl_user.sql
+++ Table Definitions/tbl_user.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_user (
     org_id UUID NOT NULL REFERENCES tbl_org (org_id),
     user_id UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
-    user_alias VARCHAR(15) NOT NULL,
     user_first_name VARCHAR(200) NOT NULL,
     user_last_name VARCHAR(200) NOT NULL,
     user_email_id VARCHAR(200) NOT NULL,
